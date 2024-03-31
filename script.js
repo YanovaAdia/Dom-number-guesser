d5cd4b2974fc2da17875206ae5e17e1da19299ae
Yanova <riefkyanova@gmail.com>
2024-03-31 12:38:44 +0700

    update-refactor

--- script.js
+++ script.js
@@ -6,29 +6,25 @@ console.log(secretNum);
 
 document.querySelector(".score").textContent = score;
 
+const displayMsg = (message) => {
+  document.querySelector(".message").textContent = message;
+};
+
 const gameLogic = () => {
   const guess = Number(document.querySelector(".num-input").value);
 
   if (!guess) {
-    document.querySelector(".message").textContent = `you're not guessing !!`;
+    displayMsg(`you're not guessing !!`);
   } else if (guess === secretNum) {
-    document.querySelector(".message").textContent = "We found the winner 🏆";
+    displayMsg("We found the winner 🏆");
     document.querySelector(".edit-here").textContent = guess;
     document.querySelector("body").style.background = "rgb(48, 109, 61)";
     document.querySelector(".white-box").style.width = "15rem";
     document.querySelector(".white-box").style.border =
       "rgb(48, 109, 61) solid 5px";
-  } else if (secretNum < guess) {
+  } else if (secretNum !== guess) {
     if (score > 1) {
-      document.querySelector(".message").textContent = `A little lower !!`;
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      loseHandler();
-    }
-  } else if (secretNum > guess) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = `A little higher!!`;
+      displayMsg(secretNum > guess ? "A little higher !" : "A little lower !");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
@@ -38,7 +34,7 @@ const gameLogic = () => {
 };
 
 const loseHandler = () => {
-  document.querySelector(".message").textContent = "Game Over ❌";
+  displayMsg("Game Over ❌");
   document.querySelector(".score").textContent = 0;
 };
 
